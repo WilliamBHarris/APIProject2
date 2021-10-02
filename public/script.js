b8e6eace36de24cb99bd378faa105ed23552f5e3
Will Harris <bharris010486@gmail.com>
2021-10-02 16:01:04 -0400

    MVP

--- public/script.js
+++ public/script.js
@@ -7,25 +7,42 @@ document.addEventListener('DOMContentLoaded', theFetch);
 function theFetch(){
     document.getElementById('Btn').addEventListener('click', dontRefresh => {
         dontRefresh.preventDefault();
-        let url = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&limit=25&q=`;
+
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&limit=48&q=`;
         let userInput = document.getElementById('search').value;
         url = url.concat(userInput);
-        
+        let output = document.querySelector('.output');
+
         fetch(url)
         .then(response => response.json() )
-        .then(content => {
-            let box = document.createElement('box');
+        .then(images => {       
+            console.log(images);     
+            while(output.firstChild){
+                output.removeChild(output.firstChild);
+            }
+            if(images.data.length === 0) {
+                output.innerText = ("No Results. Try searching for something else!");
+            } else {
+            for(let i = 0; i < images.data.length; i++){
             let img = document.createElement('img');
+            let link = document.createElement('a');
 
-            for(i = 0; i <= 24; i++){
-            img.src = content.data[i].images.original.url;
-            console.log(img.src)
-           
-            box.appendChild(img);}
+            let imgLink = images.data[i].images.original.url;
+            let bigImage = imgLink;
+
             
-            let output = document.querySelector('.output');
-            output.style.border = '1px solid black';
-            output.insertAdjacentElement('afterbegin', box);
+            img.src = images.data[i].images.fixed_width_small.url;
+            img.href = imgLink;  
+                  
+            
+            output.appendChild(link);
+            link.appendChild(img);
+            
+        
+            link.href = imgLink;
+            
+            output.style.border = '1px solid black';}
+            } 
         })
     })
 }
